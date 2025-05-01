67e215d052e66feb1aa43775592065d4fb80d83b
Jamie <jamiecate1992@gmail.com>
2025-05-01 15:39:42 +0100

    footer built

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,87 @@
 <footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+  <div class="container footer-container">
+    <img src="https://files.ekmcdn.com/autopaints/resources/design/logo.png?v=e2163789-fd68-4a12-8f19-78073eba64a7"
+      alt="">
+    <div class="slider_wrapper">
+      <ul class="footer-link-list">
+        <li><a href="index.asp">Home</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="contact-us-2-w.asp">Contact Us</a></li>
+        <li><a href="find-a-car-paint-code-3-w.asp">Find A Car Paint Code</a></li>
+        <li><a href="privacy-and-security-4-w.asp">Privacy and Security</a></li>
+        <li><a href="deliverycollection-info-12-w.asp">Delivery/Collection Info</a></li>
+        <li><a href="2k--cellulose-paint-14-w.asp">2K &amp; Cellulose Paint</a></li>
+        <li><a href="trade-account-15-w.asp">Trade Account</a></li>
+        <li><a href="/terms.asp">Terms and conditions</a></li>
+        <li><a href="/privacy.asp">Privacy</a></li>
+        <li><a href="/sitemap.asp">Sitemap</a></li>
+      </ul>
+    </div>
+    <div class="footer-block footer-block--address text-center">
+      <p>Autopaints Brighton, 119 Portland Road, Hove, BN3 5DP</p>
+      <p>info@autopaintsbrighton.co.uk</p>
+      <ul class="footer-contact">
+        <li><a class="flex flex_align-center" href="tel:01273 326981" target="_blank">01273 326981</a></li>
+      </ul>
+    </div>
+    <div class="footer-block footer-block--social">
+      <div>
+        <div class="btn-link-container"
+          style="padding:3px;display:-moz-inline-stack;display:inline-block;zoom:1;*display:inline;vertical-align: middle;">
+          <a href="https://www.facebook.com/autopaintsbrightonltd" target="_blank"><i
+              class="fa-brands fa-facebook-f"></i>Facebook</a>
+        </div>
+        <div class="btn-link-container"
+          style="padding:3px;display:-moz-inline-stack;display:inline-block;zoom:1;*display:inline;vertical-align: middle;">
+          <a href="https://www.twitter.com/autopaints" target="_blank"><i class="fa-brands fa-x-twitter"></i>X /
+            Twitter</a>
+        </div>
+        <div class="btn-link-container"
+          style="padding:3px;display:-moz-inline-stack;display:inline-block;zoom:1;*display:inline;vertical-align: middle;">
+          <a href="https://www.instagram.com/autopaintsbrighton" target="_blank"><i
+              class="fa-brands fa-instagram"></i>Instagram</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <div class="copyright-bar">
+    <div class="container">
+      <div class="copyright-block-company flex flex_align-center">
+        <div>
+
+          <span class="ekmps-vat-number">VAT Number: <span>550 2516 74</span></span><!-- /.ekmps-vat-number -->
+
+
+        </div>
+      </div>
+
+      <div class="copyright-block-sitename flex flex_align-center">
+        © Autopaints Brighton <span id="year"></span><br>
+      </div>
+
+      <div class="copyright-block-cards flex flex_align-center">
+
+        <style type="text/css">
+          .ekm-card-logos-image {
+            padding: 2px;
+          }
+        </style><span style="display:block;"><img class="ekm-card-logos-image"
+            src="https://12.cdn.ekm.net/ekmps/assets/images/cardlogos/rounded-small/maestro.png"
+            srcset="https://12.cdn.ekm.net/ekmps/assets/images/cardlogos/rounded-small/maestro@2x.png 2x"
+            alt="Pay with Maestro" width="50" height="30"><img class="ekm-card-logos-image"
+            src="https://12.cdn.ekm.net/ekmps/assets/images/cardlogos/rounded-small/visa.png"
+            srcset="https://12.cdn.ekm.net/ekmps/assets/images/cardlogos/rounded-small/visa@2x.png 2x"
+            alt="Pay with Visa" width="50" height="30"><img class="ekm-card-logos-image"
+            src="https://12.cdn.ekm.net/ekmps/assets/images/cardlogos/rounded-small/visa-electron.png"
+            srcset="https://12.cdn.ekm.net/ekmps/assets/images/cardlogos/rounded-small/visa-electron@2x.png 2x"
+            alt="Pay with Visa Electron" width="50" height="30"><img class="ekm-card-logos-image"
+            src="https://12.cdn.ekm.net/ekmps/assets/images/cardlogos/rounded-small/mastercard.png"
+            srcset="https://12.cdn.ekm.net/ekmps/assets/images/cardlogos/rounded-small/mastercard@2x.png 2x"
+            alt="Pay with Mastercard" width="50" height="30"></span>
+
+      </div>
+    </div>
+
+  </div>
 </footer>
